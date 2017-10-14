b0ed41347612460486a77347a5f7d0a78b925e2c
andrewr224 <andrew.r224@gmail.com>
2017-10-14 11:08:56 +0300

    Connect game to views

--- app.rb
+++ app.rb
@@ -3,6 +3,73 @@ require 'sinatra/reloader'
 
 require_relative 'master'
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 get '/' do
-  'Put this in your pipe & smoke it!'
+  session.delete(:game)
+  erb :index
+end
+
+post '/' do
+  session[:game] = Game.new(params[:role])
+  redirect "/#{params[:role]}"
+end
+
+get '/codebreaker' do
+  @board = session[:game].master.the_board
+  game = session[:game]
+  if game.master.game_over?
+    redirect '/game_over'
+  else
+    erb :codebreaker
+  end
+end
+
+post '/codebreaker' do
+  game = session[:game]
+  colors = []
+  colors << params[:first]
+  colors << params[:second]
+  colors << params[:third]
+  colors << params[:forth]
+  game.take_turn(colors)
+  redirect back
+end
+
+get '/codemaker' do
+  game = session[:game]
+  if defined? game.master.code
+    until game.master.game_over?
+      game.take_turn
+    end
+    redirect '/game_over'
+  else
+    erb :codemaker
+  end
+end
+
+post '/codemaker' do
+  game = session[:game]
+  colors = []
+  colors << params[:first]
+  colors << params[:second]
+  colors << params[:third]
+  colors << params[:forth]
+  game.create_code(colors)
+  redirect back
+end
+
+get '/game_over' do
+  @code = session[:game].master.code
+  @board = session[:game].master.the_board
+  game = session[:game]
+  if game.master.victory?
+    @message = "The code has been broken!"
+  else
+    @message = "The codebreaker failed."
+  end
+  erb :game_over
 end
--- master.rb
+++ master.rb
@@ -1,78 +1,42 @@
 class Game
-  def initialize(human="Obi Wan Kenobi", robot="R2-D2")
-    @human = Human.new(human)
-    @robot = Robot.new(robot)
-    start_game
+  attr_reader :master
+
+  def initialize(role)
+    @robot = Robot.new
+
+    @master = Mastermind.new(@robot.create_code) if role == 'codebreaker'
   end
 
-  def start_game
-    puts "\nLet's play Mastermind!"
-    pick_the_role
-    @master = Mastermind.new(@codemaker.create_code)
-    play
+  def create_code(*colors)
+    if colors && !colors.empty?
+      @master = Mastermind.new(colors[0])
+    else
+      @master = Mastermind.new(@robot.create_code)
+    end
   end
-
-  private
-  # human picks the role (we can refactor this into initialize)
-  def pick_the_role
-    puts "Would #{@human} be so kind as to pick the role?"
-    until @codemaker
-      puts "Please enter 'codemaker' or 'codebreaker'."
-      role = gets.chomp.downcase
-      if role == 'codemaker' || role == 'maker'
-        puts "#{@human} chose to be a 'codemaker'."
-        @codemaker = @human
-        @codebreaker = @robot
-      elsif role == 'codebreaker' || role == 'breaker'
-        puts "#{@human} chose to be a 'codebreaker'."
-        @codemaker = @robot
-        @codebreaker = @human
-      elsif role == '42'
-        puts "Ah, you're wise for a human. And yet, do select your role."
-      else
-        puts "Can't understand you, please try again."
-      end
+  # one need to take turns
+  def take_turn(*colors)
+    if colors && !colors.empty?
+      @master.guess(colors[0])
+    else
+      @master.guess(@robot.guess)
+      @robot.get_the_score(@master.score)
     end
   end
 
-  # player plays playfully here
-  def play
-    take_turn until @master.game_over?
-    @master.show_board
-    @robot.speak
-  end
-
-  # one need to take turns
-  def take_turn
-    puts @master.show_board
-    puts "\nPick your colors (one by one):"
-    puts Mastermind.show_colors.join(", ")
-
-    @master.guess(@codebreaker.enter_colors)
-    @robot.get_the_score(@master.score) if @codebreaker == @robot
-  end
 end
 
 # Mastermind class handles the mastermind thingy itself
 class Mastermind
-  # map of colors
   @@colors = ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"]
 
-  attr_reader :score
+  attr_reader :the_board, :score, :code
   def initialize(code)
     @code = code
     @turns = 0
     @the_board = []
     @last_guess = []
     @score = []
-  end
-
-  def show_board
-    @the_board.each do |c, s|
-      print "\n " + c.join("\t")
-      print "\t" + s.join(' ')
-    end
-    puts
   end
 
   # has to be a class method, as we need it befor instantiating Mastermind
@@ -83,15 +47,12 @@ class Mastermind
 
   # checks if the game is over
   def game_over?
-    if victory?
-      puts "The code has been broken!"
-      true
-    elsif (@turns == 12)
-      puts "The codebreaker failed."
-      true
-    else
-      false
-    end
+    return true if victory? || (@turns == 12)
+    false
+  end
+
+  def victory?
+    @code == @last_guess
   end
 
   # calls to a private method 'compare' and increments the turns variable
@@ -146,37 +107,14 @@ class Mastermind
   def add_to_the_board(input, score)
     @the_board[@turns] = [input, score]
   end
-
-  # check if the player is victorious
-  def victory?
-    @code == @last_guess
-  end
 end
 
 # Machine class represnents the computer opponent
 class Robot
-  def initialize(name)
-    @name = name
-    @vocabulary = ["(chirps his excitement)", "(beeps)", "(chirps confidently)", "bleep bleep bloop", "bleep blopp beep beep", "beep beep"]
+  def initialize
     @hits = 0
     @guesses = []
     @guessing_sequence = [*0..7].shuffle
-    puts "Your opponent is #{@name}"
-    speak
-  end
-
-  def to_s
-    @name
-  end
-
-  def speak
-    puts "#{@name} - #{@vocabulary.sample}"
-  end
-
-  # method for guessting colors
-  def enter_colors
-    speak
-    guess
   end
 
   # takes the score for analysis
@@ -188,20 +126,11 @@ class Robot
 
   # creates a code for human to guess
   def create_code
-    speak
     code = []
     4.times do
       code << Mastermind.show_colors[rand(0..7)]
     end
     code
-  end
-
-  private
-  # analyzes the last move
-  def analyze
-    @hits.times do
-      @guesses << @last_guess unless @guesses.length == 4
-    end
   end
 
   def guess
@@ -218,46 +147,14 @@ class Robot
       colors = @guesses.shuffle
     end
 
-    puts "#{@name} picked #{colors.join(", ")}"
     colors
-  end
-end
-
-# Human class represents the player
-class Human
-  def initialize(name)
-    @name = name
-    puts "#{@name} has entered the game"
-  end
-
-  def to_s
-    @name
-  end
-
-  # allows a human to enter colors (and validates them using another method)
-  def enter_colors
-    colors = []
-    until colors.length == 4
-      pick = gets.chomp.downcase
-      if input_validation(pick)
-        colors << pick
-      else
-        puts "Don't know that color, sorry."
-      end
-    end
-    puts "You picked #{colors.join(", ")}"
-    colors
-  end
-
-  # allows human to create a code
-  def create_code
-    puts "#{@name}, please create your cypher. You can choose from these colors:"
-    puts Mastermind.show_colors.join(", ")
-    enter_colors
   end
 
   private
-  def input_validation(pick)
-    true if Mastermind.show_colors.include?(pick)
+  # analyzes the last move
+  def analyze
+    @hits.times do
+      @guesses << @last_guess unless @guesses.length == 4
+    end
   end
 end
